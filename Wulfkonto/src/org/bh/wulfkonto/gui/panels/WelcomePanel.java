541cd1bd2cc101a3efd9ba19f2ec03c3662a4035
Supuhstar <Supuhstar@168.26.205.113>
2013-09-09 15:38:41 -0400

    initial commit

---
+++ Wulfkonto/src/org/bh/wulfkonto/gui/panels/WelcomePanel.java
@@ -0,0 +1,13 @@
+package org.bh.wulfkonto.gui.panels;
+
+import javax.swing.JPanel;
+
+/**
+ * WelcomePanel, made for Wulfkonto, is copyright Blue Husky Programming ©2013 GPL v3
+ * @author Supuhstar of Blue Husky Programming
+ * @version 1.0.0
+ * @since 2013-09-09
+ */
+public class WelcomePanel extends JPanel {
+    
+}
