8ab7c7386e077b685813f2e71d0ba5a7a83be61f
Supuhstar <Supuhstar@168.26.142.181>
2013-09-09 14:10:45 -0400

    Original Commit

---
+++ Wulfkonto/src/org/bh/wulfkonto/Main.java
@@ -0,0 +1,18 @@
+package org.bh.wulfkonto;
+
+/**
+ * Main, made for Wulfkonto, is copyright Blue Husky Programming ©2013
+ * @author Supuhstar of Blue Husky Programming
+ * @version 1.0.0
+ * @since 2013-09-09
+ */
+public class Main
+{
+	/**
+	 * @param args the command line arguments
+	 */
+	public static void main(String[] args)
+	{
+		// TODO code application logic here
+	}
+}
