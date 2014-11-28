f77d6cf71496182f5b4ccc140e6d704235eb6345
Supuhstar <supuhstar0@gmail.com>
2014-11-28 01:28:24 -0500

    Removing external code
    Preparing to make MVP

--- Wulfkonto/src/org/bh/wulfkonto/gui/GUI.java
+++ Wulfkonto/src/org/bh/wulfkonto/gui/GUI.java
@@ -4,11 +4,6 @@ import bht.tools.fx.LookAndFeelChanger;
 import java.awt.HeadlessException;
 import java.awt.Label;
 import javax.swing.JApplet;
-import org.lobobrowser.html.HtmlRendererContext;
-import org.lobobrowser.html.UserAgentContext;
-import org.lobobrowser.html.gui.HtmlPanel;
-import org.lobobrowser.html.test.SimpleHtmlRendererContext;
-import org.lobobrowser.html.test.SimpleUserAgentContext;
 
 /**
  * GUI, made for Wulfkonto, is copyright Blue Husky Programming, ©2013 GPL v3<HR/>
@@ -19,23 +14,14 @@ import org.lobobrowser.html.test.SimpleUserAgentContext;
  */
 public class GUI extends JApplet
 {
-
 	public GUI() throws HeadlessException
 	{
 		initGUI();
 	}
-
-        private HtmlPanel htmlPanel;
+	
 	private void initGUI()
 	{
 		LookAndFeelChanger.setLookAndFeel(LookAndFeelChanger.SYSTEM);
 		add(new Label("Wulfkonto"));
-                
-                htmlPanel = new HtmlPanel();
-                UserAgentContext uac = new SimpleUserAgentContext();
-                HtmlRendererContext hrc = new SimpleHtmlRendererContext(htmlPanel, uac);
-                htmlPanel.setHtml("Hello world", null, hrc);
-                add(htmlPanel);
 	}
-	
 }
