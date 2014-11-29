f3de2aff4db904b9b7e8273766aa77715ee08e58
Supuhstar <supuhstar0@gmail.com>
2014-11-29 15:40:49 -0500

    Added AppConstants

---
+++ Wulfkonto NetBeans Project/src/org/bh/wulfkonto/AppConstants.java
@@ -0,0 +1,18 @@
+package org.bh.wulfkonto;
+
+import bht.tools.util.upd.Version;
+import bht.tools.util.upd.Version.Channel;
+
+/**
+ * AppConstants, made for Wulfkonto NetBeans Project, is copyright Blue Husky Programming ©2014 GPLv3 <hr/>
+ * 
+ * @author Kyli of Blue Husky Programming
+ * @version 1.0.0
+ *		- 2014-11-29 (1.0.0) - Kyli created AppConstants
+ * @since 2014-11-29
+ */
+public interface AppConstants
+{
+	public static final String APP_NAME = "Wulfkonto";
+	public static final Version APP_VERSION = new Version(0,0,0).setChannel(Channel.LAMBDA);
+}
