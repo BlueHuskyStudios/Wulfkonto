b0aa2ada4ecd70f2ffc6e34793625a0f6347f845
Supuhstar <supuhstar0@gmail.com>
2014-11-30 22:33:58 -0500

    Added APP_WEBSITE, updated UserAgent

--- Wulfkonto NetBeans Project/src/org/bh/wulfkonto/AppConstants.java
+++ Wulfkonto NetBeans Project/src/org/bh/wulfkonto/AppConstants.java
@@ -2,19 +2,21 @@ package org.bh.wulfkonto;
 
 import bht.tools.net.UserAgent;
 import bht.tools.net.upd.Version;
-import bht.tools.net.upd.Version.Channel;
+import static bht.tools.net.upd.Version.Channel.LAMBDA;
 
 /**
  * AppConstants, made for Wulfkonto NetBeans Project, is copyright Blue Husky Programming ©2014 GPLv3 <hr/>
  * 
  * @author Kyli of Blue Husky Programming
  * @version 1.0.0
+ *		- 2014-11-29 (1.1.0) - Kyli added USER_AGENT and APP_WEBSITE
  *		- 2014-11-29 (1.0.0) - Kyli created AppConstants
  * @since 2014-11-29
  */
 public interface AppConstants
 {
-	public static final String APP_NAME = "Wulfkonto";
-	public static final Version APP_VERSION = new Version(0,0,0).setChannel(Channel.LAMBDA);
-	public static final UserAgent USER_AGENT = new UserAgent(APP_NAME, APP_VERSION);
+	public static final String    APP_NAME = "Wulfkonto";
+	public static final Version   APP_VERSION = new Version(0,0,0).setChannel(LAMBDA);
+	public static final String    APP_WEBSITE = "http://Prog.BHStudios.org/Wulfkonto/";
+	public static final UserAgent USER_AGENT = new UserAgent(APP_NAME, APP_VERSION, "+" + APP_WEBSITE);
 }
